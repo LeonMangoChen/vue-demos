8e3e4da9dca083250baa7758d8dee0d8871b067b
LeonMangoChen <leonmangochen@163.com>
2018-06-19 20:53:42 +0800

    todo-list

---
+++ work/todo_list/src/components/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <todo-header></todo-header>
+        <todo-main :todos="todos"></todo-main>
+        <todo-footer></todo-footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import todoHeader from './todoHeader'
+import todoMain from './todoMain'
+import todoFooter from './todoFooter'
+export default {
+  data () {
+    return {
+      todos: [{isDone: false, title: '吃饭'}, {isDone: true, title: '睡觉'}]
+    }
+  },
+  components: {
+    todoHeader,
+    todoMain,
+    todoFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+.todo-container{
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap{
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ work/todo_list/src/components/todoMain.vue
@@ -0,0 +1,63 @@
+<template>
+    <ul class="todo-main">
+        <li>
+            <label>
+                <input type="checkbox">
+                <span>xxxxx</span>
+            </label>
+            <button class="btn btn-danger" style="display:none">删除</button>
+        </li>
+        <li>
+            <label>
+                <input type="checkbox">
+                <span>yyyyy</span>
+            </label>
+            <button class="btn btn-danger" style="display:none">删除</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.todo-main{
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding:0px;
+}
+.todo-empty{
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+
+li{
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+}
+li label{
+    float: left;
+    cursor: pointer;
+}
+li button{
+    float: left;
+    display: none;
+    margin-top: 3px;
+}
+li:before{
+    content:inherit;
+}
+li:last-child{
+    border-bottom: none;
+}
+</style>
